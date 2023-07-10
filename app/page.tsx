3fe9d3026042525a0a353412979878708971581e
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-07-10 19:47:33 -0400

    add task component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import AddTask from './components/AddTask'
+
 const Home = () => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='text-center my-5 gap-4'>
-        <h1 className='text-2xl font-bold'>To Do List App</h1>
+        <h1 className='text-2xl font-bold overline underline decoration-pink-500'>To Do List App</h1>
+        <AddTask />
       </div>
     </main>
   )
