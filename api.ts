f08281d49b112d3e631a8f487c2706aab20b5826
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-07-11 02:22:14 -0400

    add new task modal

--- api.ts
+++ api.ts
@@ -3,7 +3,19 @@ import { ITask } from './types/tasks'
 const baseUrl = 'http://localhost:8080'
 
 export const getAllToDos = async (): Promise<ITask[]> => {
-  const response = await fetch(`${baseUrl}/tasks`)
+  const response = await fetch(`${baseUrl}/tasks`, { cache: 'no-store' })
   const todos = await response.json()
   return todos
+}
+
+export const addToDo = async (todo: ITask): Promise<ITask> => {
+  const response = await fetch(`${baseUrl}/tasks`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(todo),
+  })
+  const newToDo = await response.json()
+  return newToDo
 }
